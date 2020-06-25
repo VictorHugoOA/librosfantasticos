c63da660bbc89907010865dd846b8d5c271a0875
VictorHugoOA <49003557+VictorHugoOA@users.noreply.github.com>
2020-06-24 19:42:28 -0500

    Biblioteca y base de datos
    alaverga

--- librosFantasticos/src/app/components/Services/libros.service.ts
+++ librosFantasticos/src/app/components/Services/libros.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibrosService {
 
+  /*
   private Libro:libro[]=[
     {
       nombre:"Harry Potter y la piedra Filosofal",
@@ -68,16 +71,29 @@ export class LibrosService {
     }
  
  
-  ]
-    constructor() { }
-    getLibros():libro[]{
-      return this.Libro;
+  ]*/
+
+  productList: AngularFirestoreCollection<any>;
+  products$: Observable<libro[]>;
+
+  //Agregamos modulo de firesotre para conectarnos a la base
+    constructor(private firestore: AngularFirestore) { }
+    getLibros(){
+      this.productList = this.firestore.collection('libros');
+      this.products$ = this.productList.valueChanges();
+      return this.productList;
+    }
+
+    insertLibro(libros: libro)
+    {
+      this.productList.add({nombre:libros.nombre, autor: libros.autor, img: libros.img, sinopsis: libros.sinopsis});
     }
   }
   
-  export interface libro{
-    nombre:string,
-    autor:string,
-    img:string,
-    sinopsis:string,
+  export class libro{
+    $key: string;
+    nombre:string;
+    autor:string;
+    img:string;
+    sinopsis:string;
   }
