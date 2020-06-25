a601587927da4f832b62d9cfa65628196c573ae3
VictorHugoOA <49003557+VictorHugoOA@users.noreply.github.com>
2020-06-25 17:33:54 -0500

    Search Engine
    API Rest with search engine functioning

--- firebaseapi/src/index.js
+++ firebaseapi/src/index.js
@@ -11,19 +11,68 @@ const db = admin.firestore();
 app.listen(app.get('port'));
 
 app.get('/', bookRoute);
-app.get('/add-book', (req, res) => {
+app.post('/add-book', (req, res) => {
     const newBook = {
-        nombre: req.query.nombre,
-        autor: req.query.autor,
-        img: req.query.img,
-        sinopsis: req.query.sinopsis
+        nombre: req.post.nombre,
+        autor: req.post.autor,
+        img: req.post.img,
+        sinopsis: req.post.sinopsis
     };
     db.collection('libros').add(newBook);
     res.send({ result: "Succesful" });
 })
 app.get('/get-book', (req, res) =>
 {
-    
+    let books = db.collection('libros');
+    let val = new Array();
+    let query = null;
+    let limitSize = 10000000000;
+    if(req.query.limit)
+    {
+        limitSize = parseInt(req.query.limit);
+    }
+    if(req.query.nombre)
+    {
+        let i = 0;
+        query = books.get().then(snapshot => {
+            snapshot.forEach( doc => {
+                if(doc.data().nombre.indexOf(req.query.nombre) != -1 && i < limitSize)
+                {
+                    val.push({id: doc.id, autor: doc.data().autor, img: doc.data().img, nombre: doc.data().nombre, sinopsis: doc.data().sinopsis});
+                    i++;
+                }
+            })
+        })
+    }
+    else if(req.query.autor)
+    {
+        let i = 0;
+        query = books.get().then(snapshot => {
+            snapshot.forEach( doc => {
+                if(doc.data().autor.indexOf(req.query.autor) != -1 && i < limitSize)
+                {
+                    val.push({id: doc.id, autor: doc.data().autor, img: doc.data().img, nombre: doc.data().nombre, sinopsis: doc.data().sinopsis});
+                    i++;
+                }
+            })
+        })
+    }
+    else{
+        let i = 0;
+        query = books.get().then(snapshot => {
+            snapshot.forEach( doc => {
+                if(i < limitSize)
+                {
+                    val.push({id: doc.id, autor: doc.data().autor, img: doc.data().img, nombre: doc.data().nombre, sinopsis: doc.data().sinopsis});
+                    i++;
+                }
+            })
+        })
+    }
+    query.finally(()=>
+    {
+        res.send(val);
+    })
 });
 
 console.log("Server on port: ", app.get('port'));
