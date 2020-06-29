751ae8ec11a907c9a40dcc7eb68945c26c5439b0
NRekits <64814187+NRekits@users.noreply.github.com>
2020-06-28 20:52:53 -0500

    Revision checkbox

--- librosFantasticos/src/app/components/libro-info/libro-info.component.ts
+++ librosFantasticos/src/app/components/libro-info/libro-info.component.ts
@@ -11,14 +11,29 @@ import { Observable } from 'rxjs';
 export class LibroInfoComponent implements OnInit {
   Libros:libro[];
   milibro: libro;
-  posicion:number=5;
+  posicion:number;
+  bandera:boolean;
   constructor( public activedroute: ActivatedRoute, private Libro:LibrosService) { 
     //recuperar posicióm
-    /*this.activedroute.params.subscribe(params => {
+    this.activedroute.params.subscribe(params => {
       this.posicion=params['key'];
-    });*/
+    });
+    this.bandera=false;
   }
+  option(){
+    if($('#fisico').is(':checked') || $('#electronico').is(':checked')){
 
+    }else{
+    this.bandera=true;
+    }
+  }
+  revisar():boolean{
+    if(this.milibro.fisico > 0 || this.milibro.electronico){
+      return true;
+    }else{
+     return false;
+    }
+  }
   ngOnInit(): void {
     this.Libro.getLibros().snapshotChanges().subscribe((data)=>
     {
@@ -31,6 +46,8 @@ export class LibroInfoComponent implements OnInit {
           x.autor = element.payload.doc.data().autor;
           x.sinopsis = element.payload.doc.data().sinopsis;
           x.img = element.payload.doc.data().img;
+          x.fisico = element.payload.doc.data().fisico;
+          x.electronico = element.payload.doc.data().electronico;
           this.Libros.push(x);
         })
         this.milibro=new libro();
