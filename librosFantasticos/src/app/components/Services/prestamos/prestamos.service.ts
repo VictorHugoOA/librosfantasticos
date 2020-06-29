d9c821678cca9267d0db777316a56a3fbdcd3e76
VictorHugoOA <49003557+VictorHugoOA@users.noreply.github.com>
2020-06-29 02:46:52 -0500

    Ya se añadieron las graficas

--- librosFantasticos/src/app/components/Services/prestamos/prestamos.service.ts
+++ librosFantasticos/src/app/components/Services/prestamos/prestamos.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrestamosService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
+
+  getDiasPrestamo()
+  {
+    return this.db.collection('dias');
+  }
+
 }
--- librosFantasticos/src/app/components/grafica/grafica.component.ts
+++ librosFantasticos/src/app/components/grafica/grafica.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
+import { PrestamosService } from '../Services/prestamos/prestamos.service';
 
 @Component({
   selector: 'app-grafica',
@@ -8,54 +9,64 @@ import { Chart } from 'chart.js';
 })
 export class GraficaComponent implements OnInit {
 
-  private chart: any = null;
-  private label:string[]= ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
-  private dat:number[]=[12, 19, 3, 5, 2, 3];
-  constructor() {}
+  chart: any = null;
+  private label: string[] = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+  private dat: number[] = [12, 19, 3, 5, 2, 3];
+  private meses: string[] = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+  constructor(private prestamos: PrestamosService) { }
 
   ngOnInit(): void {
 
-    this.chart = new Chart("my chart", {
-      type: 'line',
-      data: {
-        labels: this.label,
-        datasets: [
-          {
-            label: 'No. de votos',
-            data: this.dat,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        responsive:true,
-        maintainAspectRatio: false,
-        scales: {
-          yAxes: [
+    this.prestamos.getDiasPrestamo().snapshotChanges().subscribe((data) => {
+      this.dat = [];
+      this.label = [];
+
+      data.forEach(el => {
+        this.dat.push(el.payload.doc.data()['prestamos']);
+        this.label.push(el.payload.doc.data()['dia'] + ' de' + this.meses[el.payload.doc.data()['mes']]);
+      });
+      this.chart = new Chart("my chart", {
+        type: 'line',
+        data: {
+          labels: this.label,
+          datasets: [
             {
-              ticks: {
-                beginAtZero: true,
-              },
+              label: 'No. de votos',
+              data: this.dat,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
             },
           ],
         },
-      },
-    });
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+      });
+    })
   }
 }
