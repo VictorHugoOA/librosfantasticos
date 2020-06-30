5a36545e2bb5b97e832ee2d279375ccbd908b254
NRekits <64814187+NRekits@users.noreply.github.com>
2020-06-29 19:44:55 -0500

    Actualización

--- firebaseapi/src/routes/books.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const admin = require('firebase-admin');
-router.get('/', (req, res) => {
-    console.log("Index Works");
-    res.send({result: "holaaaaa"});
-})
-
-module.exports = router;
